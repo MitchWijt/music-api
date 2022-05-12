844c538a6d2f8d50dabea6652d95706e3b974a5e
MitchWijt <mitchelwijt@gmail.com>
2022-05-12 09:31:44 +0200

    feat: add AWS CDK deployment setup

---
+++ cdk/stack.ts
@@ -0,0 +1,28 @@
+import * as cdk from "aws-cdk-lib"
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs"
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apiGateway from "aws-cdk-lib/aws-apigateway"
+import { join } from 'path'
+
+export class Stack extends cdk.Stack {
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const lambdaFunction = new NodejsFunction(this, "music-api-gql", {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            handler: "graphqlHandler",
+            entry: join(__dirname, '../src/handler.ts')
+        })
+
+        new apiGateway.LambdaRestApi(this, "music-api-gatway", {
+            handler: lambdaFunction
+        })
+    }
+}
+
+const app = new cdk.App()
+
+new Stack(app, "my-cdk-stack", {
+    stackName: 'my-cdk-stack'
+})
+
